f1fe18dae8d4d445a5f585d11be76c78803d33cc
ericng349 <45465327+ericng349@users.noreply.github.com>
2020-07-22 19:49:27 -0700

    updated contact-us page

--- client/src/components/ContactUs.js
+++ client/src/components/ContactUs.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Typography, Input, createMuiTheme, makeStyles, TextField} from '@material-ui/core';
 import '../CSS/ContactUs.css';
+import TextareaAutosize from "@material-ui/core/TextareaAutosize";
 
 const useStyles = makeStyles((theme) => ({
     form: {
@@ -20,40 +21,62 @@ const ContactUs = (props) =>{
         </Typography>
         <form className={classes.form}>
             <div class="grid-container">
-            <div class="name-sections">
-            <TextField
-                className="namesBox"
-                required
-                type="text"
-                label="First Name"
-                placeholder="John"
-                color="secondary"
-                variant="outlined"
-                size="small"
-            ></TextField>
-            <TextField
-                className="namesBox"
-                required
-                type="text"
-                label="Last Name"
-                placeholder="Doe"
-                color="secondary"
-                variant="outlined"
-                size="small"
-            />
-            </div>
-            <div id="emailBox">
-            <TextField
-                id="emailBox"
-                required
-                type="text"
-                label="Email"
-                placeholder="JohnDoe345@gmail.com"
-                color="secondary"
-                variant="outlined"
-                size="small"
-            />
-            </div>
+                <div class="name-sections">
+                    <TextField
+                        className="namesBox"
+                        required
+                        id = "firstName"
+                        type="text"
+                        label="First Name"
+                        placeholder="John"
+                        color="secondary"
+                        variant="outlined"
+                        size="small"
+                    ></TextField>
+                    <TextField
+                        className="namesBox"
+                        id = "lastName"
+                        required
+                        type="text"
+                        label="Last Name"
+                        placeholder="Doe"
+                        color="secondary"
+                        variant="outlined"
+                        size="small"
+                    />
+                </div>
+                <br></br>
+                <TextField
+                    id="emailBox"
+                    required
+                    type="text"
+                    label="Email"
+                    placeholder="JohnDoe345@gmail.com"
+                    color="secondary"
+                    variant="outlined"
+                    size="small"
+                />
+                <br></br>
+                <TextField
+                    id="subjectBox"
+                    required
+                    type="text"
+                    label="Subject"
+                    placeholder="subject"
+                    color="secondary"
+                    variant="outlined"
+                    size="small"
+                />
+
+                <br></br>
+                <TextField
+                    id="filled-multiline-static"
+                    label="Message"
+                    multiline
+                    rows={4}
+                    variant="filled"
+                    required
+                />
             </div>
             
             {/*
