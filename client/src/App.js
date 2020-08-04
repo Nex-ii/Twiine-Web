3f4b5a16950467a0bcbed3533233db4cd1c14d04
petre24 <pm_quin_24@yahoo.com>
2020-08-04 16:39:26 -0700

    added Autocomplete for industry features

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import {BrowserRouter as Router, Route } from "react-router-dom";
-import './App.css';
-import './styles/main.scss'
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import "./styles/main.scss";
 
-import AboutUs from './components/AboutUs.js';
-import ContactUs from './components/ContactUs.js';
-import NavBar from './components/NavBar.js';
-import HomePage from './components/HomePage.js';
-import Login from './components/Login.js';
-import RegisterAcc from './components/RegisterAcc.js';
-// import ForgotPassword from './components/ForgotPassword.js';
-
+import AboutUs from "./components/AboutUs.js";
+import ContactUs from "./components/ContactUs.js";
+import NavBar from "./components/NavBar.js";
+import HomePage from "./components/HomePage.js";
+import Login from "./components/Login.js";
+import RegisterAcc from "./components/RegisterAcc.js";
+// import ForgotPassword from "./components/ForgotPassword.js";
 
 function App() {
   //rce for components
@@ -18,17 +17,16 @@ function App() {
 
   return (
     <Router>
-    <div className="App">
-        <NavBar/>
+      <div className="App">
+        <NavBar />
         <Route path="/" exact component={HomePage} />
         <Route path="/about" exact component={AboutUs} />
         <Route path="/contact" exact component={ContactUs} />
-        <Route path="/login" exact component={Login}/>
-        <Route path="/register" exact component={RegisterAcc}/>
-    </div>
+        <Route path="/login" exact component={Login} />
+        <Route path="/register" exact component={RegisterAcc} />
+      </div>
     </Router>
   );
-
 }
 
 export default App;
